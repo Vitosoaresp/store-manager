aa4cd274509c1224fd1f99763096c9f886dfdc62
Vitor Soares <pereiravitor1218@gmail.com>
2022-08-11 16:16:50 -0300

    criando o Crontroller de products

---
+++ controllers/productController.js
@@ -0,0 +1,18 @@
+const productServices = require('../services/productServices');
+
+const getAll = async (req, res) => {
+  const { code, data } = await productServices.getAll();
+  return res.status(code).json(data);
+};
+
+const getById = async (req, res) => {
+  const { code, data, message } = await productServices.getById(req.params);
+
+  if (message) {
+    return res.status(code).json({ message });
+  }
+
+  return res.status(code).json(data);
+};
+
+module.exports = { getAll, getById };
