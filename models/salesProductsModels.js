6772cb57c4cd2e03fbf95e68ea85212ec4639791
Vitor Soares <pereiravitor1218@gmail.com>
2022-08-12 23:25:22 -0300

    models para sales e salesProducts

---
+++ models/salesProductsModels.js
@@ -0,0 +1,11 @@
+const connection = require('./connection');
+
+const create = async ({ saleId, productId, quantity }) => {
+  await connection.execute(
+    'INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity) VALUES (?, ?, ?)',
+    [saleId, productId, quantity],
+  );
+  return true;
+};
+
+module.exports = { create };
