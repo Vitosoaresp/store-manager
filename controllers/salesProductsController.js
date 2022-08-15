a5940688957c9186df729c5e237880ac4edd19fb
Vitor Soares <pereiravitor1218@gmail.com>
2022-08-15 16:12:03 -0300

    controller para buscar as vendas

--- controllers/salesProductsController.js
+++ controllers/salesProductsController.js
@@ -1,5 +1,18 @@
 const salesProductsServices = require('../services/salesProductsServices');
 
+const getAll = async (req, res) => {
+  const { code, data } = await salesProductsServices.getAll();
+  return res.status(code).json(data);
+};
+
+const getById = async (req, res) => {
+  const { code, data, message } = await salesProductsServices.getById(req.params);
+  if (message) {
+    return res.status(code).json({ message });
+  }
+  return res.status(code).json(data);
+};
+
 const create = async (req, res) => {
   const { message, code, data } = await salesProductsServices.create(req.body);
   if (message) {
@@ -8,4 +21,4 @@ const create = async (req, res) => {
   return res.status(code).json(data);
 };
 
-module.exports = { create };
+module.exports = { create, getAll, getById };
