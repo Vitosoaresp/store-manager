573d3fd2ebeda57f338d6a0ccf1342d8a7cb8542
Vitor Soares <pereiravitor1218@gmail.com>
2022-08-15 16:11:40 -0300

    models para buscar as vendas

--- models/salesProductsModels.js
+++ models/salesProductsModels.js
@@ -1,5 +1,28 @@
 const connection = require('./connection');
 
+const getAll = async () => {
+  const [rows] = await connection.execute(
+    `SELECT id AS saleId, product_id AS productId, date, quantity
+    FROM StoreManager.sales_products
+    INNER JOIN StoreManager.sales
+    ON StoreManager.sales_products.sale_id = StoreManager.sales.id
+    ORDER BY sale_id, product_id`,
+  );
+  return rows;
+};
+
+const getById = async ({ id }) => {
+  const [rows] = await connection.execute(
+    `SELECT product_id AS productId, date, quantity
+    FROM StoreManager.sales
+    JOIN StoreManager.sales_products
+    ON StoreManager.sales_products.sale_id = StoreManager.sales.id
+    WHERE StoreManager.sales.id = ?`,
+    [id],
+  );
+  return rows;
+};
+
 const create = async ({ saleId, productId, quantity }) => {
   await connection.execute(
     'INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity) VALUES (?, ?, ?)',
@@ -8,4 +31,4 @@ const create = async ({ saleId, productId, quantity }) => {
   return true;
 };
 
-module.exports = { create };
+module.exports = { create, getAll, getById };
