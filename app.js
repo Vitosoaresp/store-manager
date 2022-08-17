36d97d587c95d6d8829bd437edbb705a4429b7d1
Vitor Soares <pereiravitor1218@gmail.com>
2022-08-17 15:13:49 -0300

    Feat: rotas de buscar por query para produtos e de atualizar uma venda

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.get('/', (_request, response) => {
   response.send();
 });
 
+app.get('/products/search', rescue(productController.getByQuery));
 app.get('/products/:id', rescue(productController.getById));
 app.get('/products', rescue(productController.getAll));
 app.post('/products', rescue(productController.create));
@@ -20,6 +21,7 @@ app.delete('/products/:id', rescue(productController.deleteProduct));
 app.get('/sales/:id', rescue(salesProductsController.getById));
 app.get('/sales', rescue(salesProductsController.getAll));
 app.post('/sales', rescue(salesProductsController.create));
+app.put('/sales/:id', rescue(salesProductsController.update));
 app.delete('/sales/:id', rescue(salesProductsController.deleteSale));
 
 app.use((err, _req, res, _next) => {
